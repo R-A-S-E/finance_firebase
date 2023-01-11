434259860fcd9876ab2b4bd2aabc6f8dc72adc6e
Rafael Ebrahim <rafael.ebrahim@gmail.com>
2023-01-11 18:04:34 -0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:finance_firebase/src/app_material.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const AppMaterial());
+}
---
+++ lib/src/app_material.dart
@@ -0,0 +1,17 @@
+import 'package:finance_firebase/src/modules/home/home_page.dart';
+import 'package:flutter/material.dart';
+
+class AppMaterial extends StatelessWidget {
+  const AppMaterial({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const HomePage(),
+    );
+  }
+}
